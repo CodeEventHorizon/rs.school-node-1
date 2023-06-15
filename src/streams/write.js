a03ac2a9466bdcb122a8504f60e4966ae475ed97
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-06-15 15:33:57 +0400

    add: streams/write.js
    TODO: polish

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
-const write = async () => {
-    // Write your code here 
+import { createWriteStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = `${dirname}/files/fileToWrite.txt`;
+
+const write = async (filePathArg) => {
+  const writableStream = createWriteStream(filePathArg);
+
+  writableStream.write('Hello World!\n', 'utf-8');
+
+  writableStream.on('finish', () => {
+    console.log('wrote all data to file');
+  });
+
+  writableStream.end();
 };
 
-await write();
+await write(filePath);
