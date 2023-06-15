699012733a1ac758f9c4fe70d9eae6c34c51da55
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-06-15 12:37:46 +0400

    add: fs/read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
-const read = async () => {
-    // Write your code here 
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const filesDirectory = `${dirname}/files/fileToRead.txt`;
+
+const read = async (readFile) => {
+  try {
+    const fileContents = await fs.promises.readFile(readFile, 'utf-8');
+    console.log(fileContents);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
-await read();
+await read(filesDirectory);
