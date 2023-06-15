33a978b9192491f4b49e72119c19199d675388ea
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-06-15 13:31:59 +0400

    add: hash/calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
-const calculateHash = async () => {
-    // Write your code here 
+import { createHash } from 'node:crypto';
+import { readFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = `${dirname}/files/fileToCalculateHashFor.txt`;
+
+const calculateHash = async (filePathArg) => {
+  const fileContent = await readFile(filePath);
+  const hash = createHash('sha256');
+  hash.update(fileContent);
+  console.log(hash.digest('hex'));
 };
 
-await calculateHash();
+await calculateHash(filePath);
