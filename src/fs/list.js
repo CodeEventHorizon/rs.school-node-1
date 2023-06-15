3464a505b4aba838f1a0bff38bf2390a5563b596
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-06-15 12:24:14 +0400

    add: fs/list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
-const list = async () => {
-    // Write your code here 
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const filesDirectory = `${dirname}/files`;
+
+const list = async (files) => {
+  try {
+    const directoryContents = await fs.promises.readdir(files);
+    directoryContents.forEach((filename) => console.log(filename));
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
-await list();
+await list(filesDirectory);
