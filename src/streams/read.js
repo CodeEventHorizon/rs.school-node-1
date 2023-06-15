ed59fd2427bfcf23d7805280874fe87bc7588c1f
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-06-15 13:52:13 +0400

    add: streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
-const read = async () => {
-    // Write your code here 
+import { createReadStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = `${dirname}/files/fileToRead.txt`;
+
+const read = async (filePathArg) => {
+  return new Promise((resolve, reject) => {
+    const readStream = createReadStream(filePathArg);
+    readStream.pipe(process.stdout);
+    readStream.on('end', () => {
+      // New line
+      process.stdout.write('\n');
+      resolve();
+    });
+    readStream.on('error', (err) => {
+      reject(err);
+    });
+  });
 };
 
-await read();
+await read(filePath);
