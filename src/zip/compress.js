ac4c3e6a457333256983e96590b3990e1706572e
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-06-15 16:24:38 +0400

    add: zip/compress.js & zip/decompress.js
    needs fix: contents of decompressed files don't seem to appear

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
-const compress = async () => {
-    // Write your code here 
+import fs from 'fs';
+import zlib from 'zlib';
+import { pipeline } from 'stream';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const inputPath = `${dirname}/files/fileToCompress.txt`;
+const outputPath = `${dirname}/files/fileToDecompress.gz`;
+
+const compress = async (inputPathArg, outputPathArg) => {
+  const readStream = fs.createReadStream(inputPathArg);
+  const writeStream = fs.createWriteStream(outputPathArg);
+  const gzipStream = zlib.createGzip();
+
+  await pipeline(readStream, gzipStream, () => writeStream);
+
+  console.log(`${inputPathArg} was compressed to ${outputPathArg}`);
 };
 
-await compress();
+await compress(inputPath, outputPath);
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
-const decompress = async () => {
-    // Write your code here 
+import fs from 'fs';
+import zlib from 'zlib';
+import { pipeline } from 'stream';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const inputPath = `${dirname}/files/fileToDecompress.gz`;
+const outputPath = `${dirname}/files/fileToCompress.txt`;
+
+const decompress = async (inputPathArg, outputPathArg) => {
+  const readStream = fs.createReadStream(inputPathArg);
+  const writeStream = fs.createWriteStream(outputPathArg);
+  const gunzipStream = zlib.createGunzip();
+
+  await pipeline(readStream, gunzipStream, () => writeStream);
+
+  console.log(`${inputPathArg} was decompressed to ${outputPathArg}`);
 };
 
-await decompress();
+await decompress(inputPath, outputPath);
