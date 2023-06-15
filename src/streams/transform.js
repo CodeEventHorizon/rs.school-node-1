46aae7d224020ad09d5798edad208a0e66ccd908
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-06-15 15:50:19 +0400

    add: streams/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform } from 'stream';
+
+const reverse = new Transform({
+  transform(chunk, _, callback) {
+    const reversedChunk = chunk.toString().split('').reverse().join('');
+    this.push(`Reversed word: ${reversedChunk}\nWrite Again:\n`);
+    callback();
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+  process.stdout.write('Input a word to see its reverse: \n');
+  return new Promise((resolve) => {
+    process.stdin.pipe(reverse).pipe(process.stdout).on('finish', resolve);
+  });
 };
 
 await transform();
