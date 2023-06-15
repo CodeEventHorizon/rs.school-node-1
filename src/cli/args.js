865af6a0af9c02474c959975f695d778f56e614a
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-06-15 12:56:30 +0400

    add: cli/args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  // get the command line arguments, starting from index 2
+  const args = process.argv.slice(2);
+
+  for (let i = 0; i < args.length; i += 2) {
+    // extract the property name from the argument
+    const propName = args[i].replace('--', '');
+    // get the value for the property
+    const value = args[i + 1];
+    console.log(`${propName} is ${value}`);
+  }
 };
 
 parseArgs();
