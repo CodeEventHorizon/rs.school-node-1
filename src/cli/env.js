be0d101675a414e6e524f22051886cb12a0349ee
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-06-15 12:52:40 +0400

    add: cli/env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,21 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envs = process.env;
+  const rssEnvs = Object.keys(envs)
+    .filter((key) => key.startsWith('RSS_'))
+    .reduce((obj, key) => {
+      obj[key] = envs[key];
+      return obj;
+    }, {});
+
+  if (Object.keys(rssEnvs).length === 0) {
+    console.log('No RSS environment variables found!');
+    return;
+  }
+
+  const formattedEnvs = Object.entries(rssEnvs)
+    .map(([key, value]) => `RSS_${key}=${value}`)
+    .join('; ');
+  console.log(formattedEnvs);
 };
 
 parseEnv();
