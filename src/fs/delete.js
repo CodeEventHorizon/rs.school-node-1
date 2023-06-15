2a8ab1d7b046ce8465a2aaffe80f30aebdfc602f
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-06-15 12:20:26 +0400

    add: fs/delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
-const remove = async () => {
-    // Write your code here 
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const deleteFile = `${dirname}/files/fileToRemove.txt`;
+
+const remove = async (deleteFilename) => {
+  try {
+    await fs.promises.unlink(deleteFilename);
+    console.log(`File ${deleteFilename} has been deleted`);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
-await remove();
+await remove(deleteFile);
