844a6828ecb70d9009299079bf350163d77f4153
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-06-15 11:11:25 +0400

    remove: fs/copy.js await for sync functions until alternatives found

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -10,25 +10,25 @@ const filesCopy = 'files_copy';
 
 const copy = async () => {
   // Check if the `files` folder exists
-  const exists = await fs.existsSync(`${filePath}/${files}`);
+  const exists = fs.existsSync(`${filePath}/${files}`);
   if (!exists) {
     throw new Error('The `files` folder does not exist');
   }
 
   // Check if the `files_copy` folder already exists
-  const existsCopy = await fs.existsSync(`${filePath}/${filesCopy}`);
+  const existsCopy = fs.existsSync(`${filePath}/${filesCopy}`);
   if (existsCopy) {
     throw new Error('The `files_copy` folder already exists');
   }
 
   // Create the `files_copy` folder
-  await fs.mkdirSync(`${filePath}/${filesCopy}`);
+  fs.mkdirSync(`${filePath}/${filesCopy}`);
 
   // Recursively copy the contents of the `files` folder to the `files_copy` folder
-  for await (const file of fs.readdirSync(`${filePath}/${files}`)) {
+  for (const file of fs.readdirSync(`${filePath}/${files}`)) {
     const src = path.join(`${filePath}/${files}`, file);
     const dest = path.join(`${filePath}/${filesCopy}`, file);
-    await fs.copyFileSync(src, dest);
+    fs.copyFileSync(src, dest);
   }
 };
 
