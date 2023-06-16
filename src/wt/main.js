aa23bf096513f2abe624f336c072d88416b16f19
Nikoloz Muladze <nikoloz.9374@gmail.com>
2023-06-16 14:10:56 +0400

    add: wt/main.js & wt/worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,74 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+
+/**
+ * @desc             - Function that creates a number of worker threads,
+ *                     sends an incremental number to each worker,
+ *                     and waits for all promises to resolve.
+ * @async
+ */
 const performCalculations = async () => {
-    // Write your code here
+  // create number of worker threads (equal to the number of host machine logical CPU cores)
+  const numCores = os.cpus().length;
+  const promises = [];
+  for (let i = 0; i < numCores; i++) {
+    const workerPromise = new Promise((resolve, reject) => {
+      /**
+       * Send incremental number starting from 10 to each worker
+       *
+       * For example: on host machine with 4 cores you should create 4 workers
+       * and send 10 to first worker, 11 to second worker, 12 to third worker,
+       * 13 to fourth worker.
+       */
+
+      // Create a new worker thread with 10 + i as workerData argument
+      const worker = new Worker('./src/wt/worker.js', { workerData: 10 + i });
+
+      // Listen for messages from the worker thread
+      worker.on('message', (result) => {
+        // If message is received successfully, resolve the promise with the result
+        resolve(result);
+      });
+
+      // Listen for errors from the worker thread
+      worker.on('error', (error) => {
+        // If there is an error in the worker thread, reject the promise with the error
+        reject(error);
+      });
+
+      // Listen for exit event from the worker thread
+      worker.on('exit', (code) => {
+        // If the worker thread exits with a non-zero code, reject the promise with an error
+        if (code !== 0) {
+          reject(new Error(`Worker ${i + 1} stopped with exit code ${code}`));
+        }
+      });
+    });
+    // Push the worker promise to the promises array
+    promises.push(workerPromise);
+  }
+
+  try {
+    /*
+     * After all workers will finish, function should log array of results into console.
+     */
+
+    // Wait for all promises to resolve and store the results in an array
+    const results = await Promise.all(promises);
+    /**
+     * The results are array of objects with 2 props:
+     * @returns {Object}
+     * @prop {string}  status     - 'resolved' in case of successfully received value from worker
+     *                            - 'error' in case of error in worker
+     * @prop {number}  data       - value from worker in case of success
+     *                            - null in case of error in worker
+     */
+    console.log(results);
+  } catch (error) {
+    // If there is an error while waiting for promises to resolve, throw an error
+    throw new Error(`Thread error: ${error}`);
+  }
 };
 
+// Call the performCalculations function
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,39 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+// Node modules
+import { parentPort, workerData } from 'worker_threads';
 
+/**
+ * @desc             - Calculates the nth Fibonacci number.
+ *                     n should be received from main thread.
+ *
+ * @param {number} n - The index of the Fibonacci sequence. Must be a positive integer.
+ * @returns {number} - The value of the nth Fibonacci number.
+ */
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+/**
+ * @desc             - Sends the result of the nth Fibonacci calculation
+ *                     to the main thread using worker threads.
+ *                     If there is an error during the computation,
+ *                     it will send back an error message instead of a result.
+ *
+ * @returns {void}
+ */
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  try {
+    // n received from main thread using workerData
+    const n = workerData;
+    // console.log('sendResult ~ n:', n);
+
+    const result = nthFibonacci(n);
+    // console.log('sendResult ~ result:', result);
+
+    // Sends the result as a message to the main thread.
+    parentPort.postMessage({ status: 'resolved', data: result });
+  } catch (error) {
+    // If there's an error, send back to the main thread
+    parentPort.postMessage({ status: 'error', data: null });
+  }
 };
 
 sendResult();
