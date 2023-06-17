5f3d1b3696ffebb615fa58f95344397a2eac13b4
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-06-17 15:53:48 +0400

    update: hash/calcHash.js comments + renaming + add error handling

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,16 +1,47 @@
-import { createHash } from 'node:crypto';
-import { readFile } from 'fs/promises';
 import path from 'path';
+import fs from 'fs';
+import { createHash } from 'crypto';
 import { fileURLToPath } from 'url';
 
+/**
+ * @type {string}
+ */
 const dirname = path.dirname(fileURLToPath(import.meta.url));
+/**
+ * @type {string}
+ */
 const filePath = `${dirname}/files/fileToCalculateHashFor.txt`;
+/**
+ * @type {string}
+ */
+const ALGORITHM = 'sha256';
+/**
+ * @type {string}
+ */
+const ENCODING = 'hex';
 
-const calculateHash = async (filePathArg) => {
-  const fileContent = await readFile(filePathArg);
-  const hash = createHash('sha256');
-  hash.update(fileContent);
-  console.log(hash.digest('hex'));
+/**
+ * Calculates the hash of the file text asynchronously using a given `algorithmArg` and `encodingArg`.
+ *
+ * @async
+ * @param {string} filePathArg       The file path
+ * @returns {Promise}                Promise resolves after the hash is calculated
+ * @throws                           Throws an error if the the file cannot be read or 
+ *                                   does not exist or the algorithm is invalid. 
+ */
+const calculateHash = async (filePathArg, algorithmArg, encodingArg) => {
+  try {
+    const fileContent = await fs.promises.readFile(filePathArg);
+    const hash = createHash(algorithmArg);
+    hash.update(fileContent);
+    console.log(hash.digest(encodingArg));
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    } else {
+      throw error;
+    }
+  }
 };
 
-await calculateHash(filePath);
+await calculateHash(filePath, ALGORITHM, ENCODING);
