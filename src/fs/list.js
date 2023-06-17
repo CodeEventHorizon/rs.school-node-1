a39deb96d23698a8f677e43eac08dd30e4c4b2ad
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-06-17 15:49:52 +0400

    update: fs/list.js comments + renaming + return array of files

--- src/fs/list.js
+++ src/fs/list.js
@@ -2,16 +2,39 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const dirname = path.dirname(fileURLToPath(import.meta.url));
-const filesDirectory = `${dirname}/files`;
+/**
+ * Error text
+ * @type {string}
+ */
+const FSFAILED = 'FS operation failed';
+/**
+ * @type {string}
+ */
+const DIRNAME = path.dirname(fileURLToPath(import.meta.url));
+/**
+ * @type {string}
+ */
+const FOLDER = `${DIRNAME}/files`;
 
-const list = async (files) => {
+/**
+ * Asynchronously pushes files names to an array and logs that array
+ *
+ * @async
+ * @param {string} folderArg   The path of the folder
+ * @throws                     Will throw an error if listing fails & if `folderArg` does not exist
+ * @returns {Promise<void>}    A Promise that resolves when the directory is successfully listed
+ */
+const list = async (folderArg) => {
   try {
-    const directoryContents = await fs.promises.readdir(files);
-    directoryContents.forEach((filename) => console.log(filename));
+    const directoryContents = await fs.promises.readdir(folderArg);
+    const fileList = [];
+
+    directoryContents.forEach((filename) => fileList.push(filename));
+
+    console.log(fileList);
   } catch (error) {
-    throw new Error('FS operation failed');
+    throw new Error(FSFAILED);
   }
 };
 
-await list(filesDirectory);
+await list(FOLDER);
