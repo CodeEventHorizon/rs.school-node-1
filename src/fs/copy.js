1755d84f77893806827f2f435a408b088fd5daa2
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-06-17 15:47:49 +0400

    update: fs/copy.js comments + renaming + use async fs functions from fsPromises

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -2,34 +2,72 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const dirname = path.dirname(fileURLToPath(import.meta.url));
-const filePath = dirname;
+/**
+ * Error text
+ * @type {string}
+ */
+const FSFAILED = 'FS operation failed';
+/**
+ * @type {string}
+ */
+const DIRNAME = path.dirname(fileURLToPath(import.meta.url));
+/**
+ * @type {string}
+ */
+const COPYPATH = `${DIRNAME}/files`;
+/**
+ * @type {string}
+ */
+const PASTEPATH = `${DIRNAME}/files_copy`;
 
-const files = 'files';
-const filesCopy = 'files_copy';
+/**
+ * Checks if the file exists.
+ *
+ * @async
+ * @param {string} path The path to the file.
+ * @returns {Promise<void>}
+ */
+const fileExists = async (path) =>
+  !!(await fs.promises.stat(path).catch((e) => false));
 
-const copy = async () => {
-  // Check if the `files` folder exists
-  const exists = fs.existsSync(`${filePath}/${files}`);
-  if (!exists) {
-    throw new Error('The `files` folder does not exist');
-  }
+/**
+ * @desc                              Copies a directory and its contents to a new location.
+ *
+ * @param {string} copyPathArg        copy location
+ * @param {string} pastePathArg       paste location
+ * @throws                            If the `copyPathArg` folder does not exist or if the `pastePathArg` folder already exists.
+ * @returns {Promise<void>}           A Promise that resolves when the file is successfully renamed
+ */
+const copy = async (copyPathArg, pastePathArg) => {
+  try {
+    // Check if the `copyPathArg` folder exists
+    const copyExists = await fileExists(copyPathArg);
+    if (!copyExists) {
+      throw new Error(FSFAILED);
+    }
 
-  // Check if the `files_copy` folder already exists
-  const existsCopy = fs.existsSync(`${filePath}/${filesCopy}`);
-  if (existsCopy) {
-    throw new Error('The `files_copy` folder already exists');
-  }
+    // Check if the `pastePathArg` folder already exists
+    const pasteExists = await fileExists(pastePathArg);
+    if (pasteExists) {
+      throw new Error(FSFAILED);
+    }
 
-  // Create the `files_copy` folder
-  fs.mkdirSync(`${filePath}/${filesCopy}`);
+    // Create the `pastePathArg` folder
+    await fs.promises.mkdir(pastePathArg);
 
-  // Recursively copy the contents of the `files` folder to the `files_copy` folder
-  for (const file of fs.readdirSync(`${filePath}/${files}`)) {
-    const src = path.join(`${filePath}/${files}`, file);
-    const dest = path.join(`${filePath}/${filesCopy}`, file);
-    fs.copyFileSync(src, dest);
+    // Recursively copy the contents of the `copyPathArg` folder to the `pastePathArg` folder
+    const files = await fs.promises.readdir(copyPathArg);
+    for (const file of files) {
+      const src = path.join(copyPathArg, file);
+      const dest = path.join(pastePathArg, file);
+      await fs.promises.copyFile(src, dest);
+    }
+    // Log
+    console.log(`${pastePathArg} created successfully!`);
+  } catch (error) {
+    throw error;
   }
 };
 
-await copy();
+// Call the copy function
+await copy(COPYPATH, PASTEPATH);
