2fda51eb67e4d936969fbd09082891ddfd2f40c9
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-06-17 15:58:27 +0400

    update: streams/transform.js comments + await new Promise

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,10 @@
 import { Transform } from 'stream';
 
+/**
+ * Creates a Transform stream that writes the reversed version of each input.
+ *
+ * @type {Transform}
+ */
 const reverse = new Transform({
   transform(chunk, _, callback) {
     const reversedChunk = chunk.toString().split('').reverse().join('');
@@ -8,9 +13,15 @@ const reverse = new Transform({
   },
 });
 
+/**
+ * Asks the user to input a word and prints the reverse of each input entered.
+ *
+ * @async
+ * @returns {Promise<void>}     A Promise that resolves after data was piped through the stream
+ */
 const transform = async () => {
   process.stdout.write('Input a word to see its reverse: \n');
-  return new Promise((resolve) => {
+  await new Promise((resolve) => {
     process.stdin.pipe(reverse).pipe(process.stdout).on('finish', resolve);
   });
 };
