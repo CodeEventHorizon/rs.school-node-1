a101719c216a7ee2cb0a4441df88b907dc0c8906
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-06-17 15:59:09 +0400

    update: zip/compress.js comments + renaming + await new Promise

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,21 +1,45 @@
 import fs from 'fs';
 import zlib from 'zlib';
-import { pipeline } from 'stream';
 import path from 'path';
+import { pipeline } from 'stream';
 import { fileURLToPath } from 'url';
 
-const dirname = path.dirname(fileURLToPath(import.meta.url));
-const inputPath = `${dirname}/files/fileToCompress.txt`;
-const outputPath = `${dirname}/files/archive.gz`;
+/**
+ * @type {string}
+ */
+const DIRNAME = path.dirname(fileURLToPath(import.meta.url));
+/**
+ * @type {string}
+ */
+const INPUTPATH = `${DIRNAME}/files/fileToCompress.txt`;
+/**
+ * @type {string}
+ */
+const OUTPUTPATH = `${DIRNAME}/files/archive.gz`;
 
+/**
+ * Compresses a file to .gz
+ * @async
+ * @param {string} inputPathArg      File path to compress
+ * @param {string} outputPathArg     File path where to save compressed file
+ * @returns {Promise<void>}          A Promise that resolves after the file is compressed and rejects if there's an error.
+ * @throws                           Throws an error if an error occurs during compression.
+ */
 const compress = async (inputPathArg, outputPathArg) => {
-  const readStream = fs.createReadStream(inputPathArg);
-  const writeStream = fs.createWriteStream(outputPathArg);
-  const gzipStream = zlib.createGzip();
+  await new Promise((resolve, reject) => {
+    const readStream = fs.createReadStream(inputPathArg);
+    const writeStream = fs.createWriteStream(outputPathArg);
+    const gzipStream = zlib.createGzip();
 
-  await pipeline(readStream, gzipStream, writeStream, () => {
-    console.log(`${inputPathArg} was compressed to ${outputPathArg}`);
+    pipeline(readStream, gzipStream, writeStream, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        console.log(`${inputPathArg} was compressed to ${outputPathArg}`);
+        resolve();
+      }
+    });
   });
 };
 
-await compress(inputPath, outputPath);
+await compress(INPUTPATH, OUTPUTPATH);
