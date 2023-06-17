ea3dd0a462bbb4f95b26b05059aec64cbdad268d
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-06-17 15:51:02 +0400

    refactor: fs/rename.js comments + renaming

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -2,10 +2,33 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const dirname = path.dirname(fileURLToPath(import.meta.url));
-const oldFileName = `${dirname}/files/wrongFilename.txt`;
-const newFileName = `${dirname}/files/properFilename.md`;
+/**
+ * Error text
+ * @type {string}
+ */
+const FSFAILED = 'FS operation failed';
+/**
+ * @type {string}
+ */
+const DIRNAME = path.dirname(fileURLToPath(import.meta.url));
+/**
+ * @type {string}
+ */
+const OLDFILENAME = `${DIRNAME}/files/wrongFilename.txt`;
+/**
+ * @type {string}
+ */
+const NEWFILENAME = `${DIRNAME}/files/properFilename.md`;
 
+/**
+ * Asynchronously renames a file from `oldFileNameArg` to `newFileNameArg`
+ *
+ * @async
+ * @param {string} oldFileNameArg  The current name of the file
+ * @param {string} newFileNameArg  The new name of the file
+ * @throws                         Will throw an error if file accessing or renaming fails
+ * @returns {Promise<void>}        A Promise that resolves when the file is successfully renamed
+ */
 const rename = async (oldFileNameArg, newFileNameArg) => {
   try {
     await Promise.all([
@@ -18,8 +41,9 @@ const rename = async (oldFileNameArg, newFileNameArg) => {
     ]);
     await fs.promises.rename(oldFileNameArg, newFileNameArg);
   } catch (err) {
-    throw new Error('FS operation failed');
+    throw new Error(FSFAILED);
   }
 };
 
-await rename(oldFileName, newFileName);
+// Call the rename function
+await rename(OLDFILENAME, NEWFILENAME);
