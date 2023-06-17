1d3eb601354ca5987b798c41e28a9d694328a5ff
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-06-17 15:51:39 +0400

    chore: cli/args.js comments

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,14 +1,20 @@
+/**
+ * Parses the command line args & logs the value for each
+ *
+ * @name parseArgs
+ * @returns {void}
+ */
 const parseArgs = () => {
-  // get the command line arguments, starting from index 2
+  // command line arguments, starting from index 2
   const args = process.argv.slice(2);
 
   for (let i = 0; i < args.length; i += 2) {
-    // extract the property name from the argument
+    // remove `--`
     const propName = args[i].replace('--', '');
-    // get the value for the property
     const value = args[i + 1];
     console.log(`${propName} is ${value}`);
   }
 };
 
+// Call the parseArgs function
 parseArgs();
