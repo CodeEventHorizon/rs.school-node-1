979666f9015bf2ccb2c2e5c5e09ea6276e220c21
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-06-17 15:52:18 +0400

    update: cli/env.js comments + optimizations

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,19 +1,29 @@
+/**
+ * Retrieves all RSS env variables, formats and logs them
+ * @name parseEnv
+ * @returns {void}
+ */
 const parseEnv = () => {
-  const envs = process.env;
-  const rssEnvs = Object.keys(envs)
-    .filter((key) => key.startsWith('RSS_'))
-    .reduce((obj, key) => {
-      obj[key] = envs[key];
+  /**
+   * @type {Object.<string, string>}
+   */
+  const rssEnvs = Object.entries(process.env)
+    .filter(([key]) => key.startsWith('RSS_'))
+    .reduce((obj, [key, value]) => {
+      obj[key] = value;
       return obj;
     }, {});
 
   if (Object.keys(rssEnvs).length === 0) {
-    console.log('No RSS environment variables found!');
+    console.log('There are no RSS env variables!');
     return;
   }
 
+  /**
+   * @type {string}
+   */
   const formattedEnvs = Object.entries(rssEnvs)
-    .map(([key, value]) => `RSS_${key}=${value}`)
+    .map(([key, value]) => `${key}=${value}`)
     .join('; ');
   console.log(formattedEnvs);
 };
