cfa8a7a7ff1d2ef943bbdfb1ce306aeee69c7795
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-06-17 15:57:47 +0400

    update: streams/write.js comments + renaming + await new Promise

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,20 +1,41 @@
-import { createWriteStream } from 'fs';
+import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const dirname = path.dirname(fileURLToPath(import.meta.url));
-const filePath = `${dirname}/files/fileToWrite.txt`;
+/**
+ * @type {string}
+ */
+const DIRNAME = path.dirname(fileURLToPath(import.meta.url));
+/**
+ * @type {string}
+ */
+const FILEPATH = `${DIRNAME}/files/fileToWrite.txt`;
 
+/**
+ * Writes 'Hello World!' to a file
+ *
+ * @async
+ * @function write
+ * @param {string} filePathArg        The file path
+ * @returns {Promise<string>}         A Promise that resolves after data is written to the file. 
+ * @throws                            If there was an error writing into the file
+ */
 const write = async (filePathArg) => {
-  const writableStream = createWriteStream(filePathArg);
+  await new Promise((resolve, reject) => {
+    const writableStream = fs.createWriteStream(filePathArg);
 
-  writableStream.write('Hello World!\n', 'utf-8');
+    writableStream.write('Hello World!\n', 'utf-8');
 
-  writableStream.on('finish', () => {
-    console.log('wrote all data to file');
+    writableStream.on('finish', () => {
+      console.log('Success!');
+    });
+
+    writableStream.on('error', (err) => {
+      reject(err);
+    });
+
+    writableStream.end();
   });
-
-  writableStream.end();
 };
 
-await write(filePath);
+await write(FILEPATH);
