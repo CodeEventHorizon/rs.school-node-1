056de6fec65272dace20f17c38a61cd63f4cde5c
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-06-17 15:48:20 +0400

    refactor: fs/delete.js comments + renaming

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -2,16 +2,37 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const dirname = path.dirname(fileURLToPath(import.meta.url));
-const deleteFile = `${dirname}/files/fileToRemove.txt`;
+/**
+ * Error text
+ * @type {string}
+ */
+const FSFAILED = 'FS operation failed';
+/**
+ * @type {string}
+ */
+const DIRNAME = path.dirname(fileURLToPath(import.meta.url));
+/**
+ * @type {string}
+ */
+const DELETEFILE = `${DIRNAME}/files/fileToRemove.txt`;
 
+/**
+ * Asynchronously deletes a file `deleteFilename`
+ *
+ * @async
+ * @function remove
+ * @param {string} deleteFilename   file path that will be deleted
+ * @throws                          Will throw an error if file deletion fails & if file does not exist
+ * @returns {Promise<void>}         A Promise that resolves when the file is successfully deleted
+ */
 const remove = async (deleteFilename) => {
   try {
     await fs.promises.unlink(deleteFilename);
     console.log(`File ${deleteFilename} has been deleted`);
   } catch (error) {
-    throw new Error('FS operation failed');
+    throw new Error(FSFAILED);
   }
 };
 
-await remove(deleteFile);
+// Call the remove function
+await remove(DELETEFILE);
