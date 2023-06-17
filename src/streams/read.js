fbe2fd0a316190d358613ab04358226b9ba377f7
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-06-17 15:56:38 +0400

    update: streams/read.js comments + renaming + await new Promise

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,16 +1,31 @@
-import { createReadStream } from 'fs';
+import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const dirname = path.dirname(fileURLToPath(import.meta.url));
-const filePath = `${dirname}/files/fileToRead.txt`;
+/**
+ * @type {string}
+ */
+const DIRNAME = path.dirname(fileURLToPath(import.meta.url));
+/**
+ * @type {string}
+ */
+const FILEPATH = `${DIRNAME}/files/fileToRead.txt`;
 
+/**
+ * Reads the file asynchronously and logs the standard output.
+ *
+ * @async
+ * @param {string} filePathArg      The file path
+ * @throws                          If an error occurs during reading the file.
+ * @returns {Promise<void>}         A Promise that resolves after the file was read
+ */
 const read = async (filePathArg) => {
-  return new Promise((resolve, reject) => {
-    const readStream = createReadStream(filePathArg);
-    readStream.pipe(process.stdout);
+  await new Promise((resolve, reject) => {
+    const readStream = fs.createReadStream(filePathArg);
+    readStream.on('data', (chunk) => {
+      process.stdout.write(chunk);
+    });
     readStream.on('end', () => {
-      // New line
       process.stdout.write('\n');
       resolve();
     });
@@ -20,4 +35,4 @@ const read = async (filePathArg) => {
   });
 };
 
-await read(filePath);
+await read(FILEPATH);
