7435f1d3351931be8841cdee76ccbeac64b62cb0
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-06-17 15:50:19 +0400

    refactor: fs/read.js comments + renaming

--- src/fs/read.js
+++ src/fs/read.js
@@ -2,16 +2,36 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const dirname = path.dirname(fileURLToPath(import.meta.url));
-const filesDirectory = `${dirname}/files/fileToRead.txt`;
+/**
+ * Error text
+ * @type {string}
+ */
+const FSFAILED = 'FS operation failed';
+/**
+ * @type {string}
+ */
+const DIRNAME = path.dirname(fileURLToPath(import.meta.url));
+/**
+ * @type {string}
+ */
+const FILEPATH = `${DIRNAME}/files/fileToRead.txt`;
 
+/**
+ * Asynchronously read file and log the content
+ *
+ * @async
+ * @param {string} readFile        The path of the file
+ * @throws                         Will throw an error if reading the file fails & if file does not exist
+ * @returns {Promise<void>}        A Promise that resolves when the file is successfully read and logged to the console
+ */
 const read = async (readFile) => {
   try {
     const fileContents = await fs.promises.readFile(readFile, 'utf-8');
     console.log(fileContents);
   } catch (error) {
-    throw new Error('FS operation failed');
+    throw new Error(FSFAILED);
   }
 };
 
-await read(filesDirectory);
+// Call the read function
+await read(FILEPATH);
