77620b77ad52dc4657cd21db98b590101859716f
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-06-17 15:59:51 +0400

    update: zip/decompress.js comments + renaming + await new Promise + add error handling

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,21 +1,47 @@
 import fs from 'fs';
 import zlib from 'zlib';
-import { pipeline } from 'stream';
 import path from 'path';
+import { pipeline } from 'stream';
 import { fileURLToPath } from 'url';
 
-const dirname = path.dirname(fileURLToPath(import.meta.url));
-const inputPath = `${dirname}/files/archive.gz`;
-const outputPath = `${dirname}/files/fileToCompress.txt`;
+/**
+ * @type {string}
+ */
+const DIRNAME = path.dirname(fileURLToPath(import.meta.url));
+/**
+ * @type {string}
+ */
+const INPUTPATH = `${DIRNAME}/files/archive.gz`;
+/**
+ * @type {string}
+ */
+const OUTPUTPATH = `${DIRNAME}/files/fileToCompress.txt`;
 
+/**
+ * Decompresses a file
+ *
+ * @async
+ * @function
+ * @param {string} inputPathArg     File path to decompress
+ * @param {string} outputPathArg    File path where to save decompressed file
+ * @returns {Promise<void>}         A Promise that resolves after the file is decompressed and rejects if there's an error.
+ * @throws                          Throws an error if an error occurs during decompression.
+ */
 const decompress = async (inputPathArg, outputPathArg) => {
-  const readStream = fs.createReadStream(inputPathArg);
-  const writeStream = fs.createWriteStream(outputPathArg);
-  const gunzipStream = zlib.createGunzip();
+  await new Promise((resolve, reject) => {
+    const readStream = fs.createReadStream(inputPathArg);
+    const writeStream = fs.createWriteStream(outputPathArg);
+    const gunzipStream = zlib.createGunzip();
 
-  await pipeline(readStream, gunzipStream, writeStream, () => {
-    console.log(`${inputPathArg} was decompressed to ${outputPathArg}`);
+    pipeline(readStream, gunzipStream, writeStream, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        console.log(`${inputPathArg} was decompressed to ${outputPathArg}`);
+        resolve();
+      }
+    });
   });
 };
 
-await decompress(inputPath, outputPath);
+await decompress(INPUTPATH, OUTPUTPATH);
